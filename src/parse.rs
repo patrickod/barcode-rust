29e5843c4229c0f8e43dd8f401f346e72838a18b
Patrick O'Doherty <p@trickod.com>
2015-05-09 16:25:16 -0700

    Fix KeyEvent name resolution issue

--- src/parse.rs
+++ src/parse.rs
@@ -1,7 +1,10 @@
+extern crate num;
+use num::traits::FromPritive;
+
 use events::KeyEvent;
 use libevdev::InputEvent;
 
 pub fn parse_event(ev: &InputEvent, buf: &mut Vec<char> ) {
-    let key = KeyEvent.from_u16(ev.code);
+    let key = KeyEvent::from_u16(ev.code);
     println!("Got key: {:?}", key.to_char());
 }
