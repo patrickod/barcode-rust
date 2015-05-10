8588920494d1cbf96c7b17e1fea629b1ba4d27a2
Patrick O'Doherty <p@trickod.com>
2015-05-09 20:46:21 -0700

    The start of buffering. Doesn't work, it just panics

--- src/parse.rs
+++ src/parse.rs
@@ -1,10 +1,21 @@
 use events::KeyEvent;
 use libevdev::InputEvent;
+use std::io::stdout;
+use std::io::Write;
+
 use num::FromPrimitive;
 
-pub fn parse_event(ev: &InputEvent, buf: &mut Vec<char> ) {
+pub fn parse_event(ev: &InputEvent, buf: &mut Vec<u8> ) {
     if ev.event_type == 1 && ev.value == 1 {
         let key = KeyEvent::from_u16(ev.code).unwrap();
-        println!("Got key: {:?}", key.to_char());
+        if !key.to_char().is_empty() {
+            buf.push(key.to_char().parse::<u8>().unwrap());
+        }
+    }
+    if buf.last().unwrap() == &"\n".parse::<u8>().unwrap() {
+        let mut stdout = stdout();
+        stdout.write(buf);
+        stdout.flush();
+        buf.clear();
     }
 }
